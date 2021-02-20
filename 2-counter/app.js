475c25869f5c2671ed5b62fdade2d5c2896e0bb7
AlexJ <linctjabc@gmail.com>
2021-02-20 16:33:48 -0500

    modefy button to show companies infor!

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -3,16 +3,31 @@ let count = 2;
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+const companies = ["Uber", "Lyft", "Google", "Apple", "SpaceX", "Tinder"]
+const animals = ["Aardvark", "Blue Footed Booby", "Flying Dragon", "Giraffe Weevil", "Hammerhead Slug", "Komodo Dragon", "Naked Mole Rat", "Okapi", "Red Panda"]
+const vowels = ["A", "E", "I", "O", "U"]
+
+function addInfor(){
+  value.innerHTML = "";
+  for (let i = 0; i < 5; i++) {
+    const companyName = companies[Math.floor(Math.random() * companies.length)]
+
+    const firstLetter = companyName.charAt(0)
+    const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!<br />"
+    
+    value.innerHTML += companyString
+  }
+}
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
-      count-=99;
+      addInfor();
     } else if (styles.contains("increase")) {
-      count *= 3;
+      addInfor();
     } else {
-      count = 0;
+      value.innerHTML = "Hit a button!";
     }
 
     if (count > 0) {
@@ -24,6 +39,5 @@ btns.forEach(function (btn) {
     if (count === 0) {
       value.style.color = "#222";
     }
-    value.textContent = count;
   });
 });
